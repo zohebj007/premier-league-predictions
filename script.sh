8df92930ebab08b84bbb7ce56df6147dfd369a51
Zoheb007 <107297597+zohebj007@users.noreply.github.com>
2024-10-06 17:59:25 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+# Create a virtual environment in the Jenkins workspace
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip to the latest version
+#pip install --upgrade pip
+
+# Install the required dependencies inside the virtual environment
+pip install sagemaker boto3 scikit-learn
+
+# Run the SageMaker pipeline script
+python pipeline.py
+
+# Deactivate the virtual environment after the job is done
+deactivate
